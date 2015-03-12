fb05998093fef3eb9e6ded34cf49284120ad1145
Jill Kuchman & virginie trubiano <student@epicodus.com>
2015-03-12 11:02:40 -0700

    Add different letter with the same value test

--- tests/ScrabbleScoreTest.php
+++ tests/ScrabbleScoreTest.php
@@ -5,32 +5,46 @@
     class ScrabbleScoreTest extends PHPUnit_Framework_TestCase
     {
 
-    function test_singleLetter()
-    {
-        //arrange
-        $test_scrabble = new ScrabbleScore;
-        $input = 'A';
+        function test_singleLetter()
+        {
+            //arrange
+            $test_scrabble = new ScrabbleScore;
+            $input = 'A';
 
-        //act
-        $result = $test_scrabble->scrabble_score_calculator($input);
+            //act
+            $result = $test_scrabble->scrabble_score_calculator($input);
 
-        //assert
-        $this->assertEquals(1, $result);
-    }
+            //assert
+            $this->assertEquals(1, $result);
+        }
 
-    function test_duplicateSingleLetter()
-    {
-        //arrange
-        $test_scrabble = new ScrabbleScore;
-        $input = 'AA';
+        function test_duplicateSingleLetter()
+        {
+            //arrange
+            $test_scrabble = new ScrabbleScore;
+            $input = 'AA';
 
-        //act
-        $result = $test_scrabble->scrabble_score_calculator($input);
+            //act
+            $result = $test_scrabble->scrabble_score_calculator($input);
 
-        //assert
-        $this->assertEquals(2, $result);
+            //assert
+            $this->assertEquals(2, $result);
 
-    }
+        }
+
+        function test_differentLettersSameValue()
+        {
+            //arrange
+            $test_scrabble = new ScrabbleScore;
+            $input = 'AE';
+
+            //act
+            $result = $test_scrabble->scrabble_score_calculator($input);
+
+            //assert
+            $this->assertEquals(2, $result);
+            
+        }
 
 
 
