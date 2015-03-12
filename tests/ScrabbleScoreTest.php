416adeb5ba384d24c0aa23e6fb2f640159999bf7
Jill Kuchman & virginie trubiano <student@epicodus.com>
2015-03-12 09:58:21 -0700

    Add single letter test

---
+++ tests/ScrabbleScoreTest.php
@@ -0,0 +1,26 @@
+<?php
+
+    require_once 'src/Scrabble.php';
+
+    class ScrabbleScoreTest extends PHPUnit_Framework_TestScrabbleScore
+    {
+
+    function test_singleLetter()
+    {
+        //arrange
+        $test_scrabble = new ScrabbleScore;
+        $input = 'A';
+
+        //act
+        $result = $test_scrabble->scrabble_score_calculator($input);
+
+        //assert
+        $this->assertEquals('1', $result);
+    }
+
+
+
+
+    }
+
+?>
