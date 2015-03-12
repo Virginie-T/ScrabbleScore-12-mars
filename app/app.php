fd0b61a70c6f95ed2d10f7a7a17ef2c991d9e486
Jill Kuchman & virginie trubiano <student@epicodus.com>
2015-03-12 12:12:34 -0700

    add app dependencies for twig and silex

--- app/app.php
+++ app/app.php
@@ -3,4 +3,10 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/ScrabbleScore.php";
 
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__."/../views"
+    ));
+
 ?>
